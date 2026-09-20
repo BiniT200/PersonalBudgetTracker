f18a158f8695bf343955253285049c2610e3df62
BiniTSubedi <binitking23@gmail.com>
2026-09-20 19:40:44 +1000

    Rename and configure main application form

--- PersonalBudgetTracker/MainForm.Designer.cs
+++ PersonalBudgetTracker/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace PersonalBudgetTracker
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            SuspendLayout();
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Text = "Form1";
+            ClientSize = new Size(878, 544);
+            Name = "MainForm";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Personal Budget Tracker";
+            ResumeLayout(false);
         }
 
         #endregion
